3679402f8aa23799604e744b8c7f812b81f65d0f
Shane McCloskey <sdmccloskey@gmail.com>
2015-04-30 00:06:29 +0000

    madlibs

---
+++ height_to_cent.rb
@@ -0,0 +1,10 @@
+puts " what is your height in inches?"
+height_inches = gets.chomp
+puts "did you know your height in centimeters is :" 
+a = height_inches.to_f 
+cent = height_inches * a
+puts cent
+
+
+
+
---
+++ madlib.rb
@@ -0,0 +1,18 @@
+puts " Hello whats your name?"
+user_name = gets.chomp
+
+puts " Welcome to the I dont know what Im doing Madlib " + user_name + "!"
+
+puts "Tell me the name of someone famous"
+famous_name = gets.chomp
+
+puts "Whats your favorite fruit?"
+fruit = gets.chomp
+
+puts "Name a body part"
+bofy_part = gets.chomp
+
+puts " Tell me a color"
+color = gets.chomp
+
+puts "Thanks" + user_name 
