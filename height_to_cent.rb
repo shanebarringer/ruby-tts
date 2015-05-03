dbe228c052a2a6262e5d4832bb3edd404d96c637
Shane Barringer <shane.barringer@outlook.com>
2015-05-03 18:05:05 -0400

    updates height_inches variable

--- height_to_cent.rb
+++ height_to_cent.rb
@@ -1,9 +1,16 @@
 puts " what is your height in inches?"
-height_inches = gets.chomp
-puts "did you know your height in centimeters is :" 
+# storing user's answer in height_inches variable
+height_inches = gets.chomp.to_i.to_s #get user's answer (which is a string) and convert it to an integer
+
+
+
 a = height_inches.to_f 
 cent = height_inches * a
 puts cent
+
+puts "did you know your height in centimeters is : "
+
+
 
 
 
